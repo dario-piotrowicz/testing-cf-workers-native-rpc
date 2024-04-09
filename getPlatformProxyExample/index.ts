3246b127a82018820b29849ae4e496244b415621
Dario Piotrowicz <dario@cloudflare.com>
2024-04-09 12:50:17 +0100

    add sum-consumer service binding

--- getPlatformProxyExample/index.ts
+++ getPlatformProxyExample/index.ts
@@ -4,7 +4,11 @@ const { env, dispose } = await getPlatformProxy<Env>();
 
 const helloWorkerResp = await env.HELLO_WORKER.fetch('http://localhost');
 const helloWorkerRespText = await helloWorkerResp.text();
-console.log(`The text response from the bound hello worker is: "${helloWorkerRespText}"`);
+console.log(`The text response from the bound hello worker is: "${helloWorkerRespText}"\n`);
+
+const sumWorkerResp = await env.SUM_CONSUMER.fetch('http://localhost/1/2/3');
+const sumWorkerRespText = await sumWorkerResp.text();
+console.log(`The text response from the bound sum consumer worker is: "${sumWorkerRespText}"\n`);
 
 console.log('');
 await dispose();
